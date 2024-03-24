db1c7034d39c9d750d1c094cea612ca6f942ac2d
Daniel Moreira <134517417+DanielMoreira45@users.noreply.github.com>
2024-03-24 17:58:14 +0100

    Finalisation de Jouer sans le modele

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,30 +1,39 @@
 import 'package:go_router/go_router.dart';
+import 'package:tpnotemobile/ui/Jouer.dart';
 
 import '../app.dart';
 import '../ui/Home.dart';
 
 final router = GoRouter(
-  initialLocation: '/',
-  routes:<RouteBase>[
+  routes: [
     GoRoute(
       path: '/',
       name: 'home',
       builder: (context, state) => const HomePage(),
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'jouer',
+          name: 'jouer',
+          builder: (context, state) => const JouerPage(),
+          routes: <RouteBase>[
+            GoRoute(
+              path: ':difficulte',
+              name: 'difficulte',
+              builder: (context, state) => const JouerPage(),
+            ),
+          ]
+        ),
+      ],
     ),
     GoRoute(
       path: '/scores',
       name: 'scores',
-      builder: (context, state) => const MyHomePage(title: 'Score'),
+      builder: (context, state) => const MyHomePage(title: 'Scores'),
     ),
     GoRoute(
       path: '/regles',
       name: 'regles',
-      builder: (context, state) => const MyHomePage(title: 'Regles'),
-    ),
-    GoRoute(
-      path: '/jouer',
-      name: 'jouer',
-      builder: (context, state) => const MyHomePage(title: 'Jouer'),
+      builder: (context, state) => const MyHomePage(title: 'Règles'),
     ),
   ],
 );
